de21a7a1bb98bd3a6af3a6cefbf8b1196494a26f
Will Holmes <willholmes787@gmail.com>
2025-10-09 00:14:06 -0500

    added navlinks to header

--- src/app.jsx
+++ src/app.jsx
@@ -9,38 +9,40 @@ import { WriteStory } from './writeStory/writeStory';
 
 export default function App() {
     return (
-        <div className="body">
-            <header>
-                <div class="title-and-name">
-                    <div class="name">
-                        <p>William Holmes</p>
+        <BrowserRouter>
+            <div className="body">
+                <header>
+                    <div class="title-and-name">
+                        <div class="name">
+                            <p>William Holmes</p>
+                        </div>
+                        <div class="title">
+                            <h1>Story Forrest</h1>
+                            <img src="/Images/TreeIcon.jpeg" height="50" alt="Tree Icon"/>
+                        </div>
                     </div>
-                    <div class="title">
-                        <h1>Story Forrest</h1>
-                        <img src="/Images/TreeIcon.jpeg" height="50" alt="Tree Icon"/>
+                    <nav class="navigation">
+                        <button> 
+                            <NavLink to='login'>Login</NavLink>
+                        </button>
+                        <button> 
+                            <NavLink to='feed'>Feed</NavLink>
+                        </button>
+                        <button> 
+                            <NavLink to='feed'>Feed</NavLink>
+                        </button>
+                    </nav>
+                </header>
+                
+                <main> </main>
+                
+                <footer>
+                    <div class="git-repo-link">
+                        <p>See the source Code: </p>
+                        <a href="https://github.com/WillHolmes0/startup">Github Repository</a>
                     </div>
-                </div>
-                <nav class="navigation">
-                    <button> 
-                        <a href="index.html">Login</a>
-                    </button>
-                    <button> 
-                        <a href="home.html">Feed</a>
-                    </button>
-                    <button> 
-                        <a href="writeStory.html">Write</a>
-                    </button>
-                </nav>
-            </header>
-            
-            <main> </main>
-            
-            <footer>
-                <div class="git-repo-link">
-                    <p>See the source Code: </p>
-                    <a href="https://github.com/WillHolmes0/startup">Github Repository</a>
-                </div>
-            </footer>
-        </div>
+                </footer>
+            </div>
+        </BrowserRouter>
     );
 }
